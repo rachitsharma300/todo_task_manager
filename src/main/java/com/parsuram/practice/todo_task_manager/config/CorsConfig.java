fd7cfebeced6278e6bc55f03b35ef34bc0dd8778
rachitsharma300 <rachitsharma300@gmail.com>
2025-09-02 23:00:38 +0530

    feat: Prevent intruption on Browser

--- src/main/java/com/parsuram/practice/todo_task_manager/config/CorsConfig.java
+++ src/main/java/com/parsuram/practice/todo_task_manager/config/CorsConfig.java
@@ -1,4 +1,24 @@
 package com.parsuram.practice.todo_task_manager.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
 public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")  // allow all endpoints
+                        .allowedOrigins("http://localhost:3000", "http://127.0.0.1:3000") // frontend origin(s)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
 }
